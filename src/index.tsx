b86157a2687974d77368ab7696270dc4f78d796c
unknown <brdevservice@gmail.com>
2022-06-02 01:01:14 -0400

    Redux installation

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const store = configureStore(
+  {}, // Reducers
+  {} // Init state
+)
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Isologo from '../assets/isologo.png';
 
 export default function Login() {
 
     return(
-        <h1>Hi</h1>
+        <div className='flex items-center min-h-screen'>
+            <div className="w-full">
+                <img src={ Isologo } alt='GRAPPS' className='w-48 mb-10 mx-auto' />
+                <div className='p-6'>
+                    <h4 className="uppercase text-2xl text-center font-bold mb-4">
+                        Login
+                    </h4>
+                    <div className="w-full min:w-3/4 md:w-4/12 mx-auto px-3 py-6 bg-gray-700 text-white">
+                        <form className='flex flex-col items-center'>
+                            <div className="w-full md:w-3/4 relative mb-6">
+                                <label className="block mb-3" htmlFor="">User or Email</label>
+                                <input autoComplete='u-name' className='w-full block focus:bg-transparent valid:bg-transparent bg-transparent border-2 border-white-900 rounded p-2 text-xs' type="text" name="u-name" id="u-name" />
+                            </div>
+                            <div className="w-full md:w-3/4 relative">
+                                <label className="block mb-3" htmlFor="">Password</label>
+                                <input autoComplete='u-pass' className='w-full block focus:bg-transparent valid:bg-transparent bg-transparent border-2 border-white-900 rounded p-2 text-xs' type="password" name="pass" id="pass" />
+                            </div>
+                            <div className="block w-full md:w-3/4 mx-auto mt-4">
+                                <button type='submit' className="w-full bg-gray-800 hover:bg-gray-900 p-2">
+                                    LOGIN
+                                </button>
+                            </div>
+                            <div className="w-full md:w-3/4 grid grid-cols-2 mt-3">
+                                <Link to='' className='text-xs text-center hover:underline'>
+                                    Forgot password?
+                                </Link>
+                                <Link to='' className='text-xs text-center hover:underline'>
+                                    Need new user?
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 }
